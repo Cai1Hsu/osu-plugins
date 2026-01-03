a17a022662fad59d658d5738192f4bfe7e8bd6fb
Caiyi Hsu <Cai1Hsu@outlook.com>
2026-01-03 21:45:43 +0800

    feat: add LegacyBreakOverlay plugin and related test infrastructure

---
+++ osu.Plugin.LegacyBreakOverlay.Tests/TestSceneLegacyBreakOverlay.cs
@@ -0,0 +1,77 @@
+using osu.Framework.Allocation;
+using osu.Framework.Audio;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Rendering;
+using osu.Framework.Graphics.Shapes;
+using osu.Framework.Graphics.Textures;
+using osu.Framework.IO.Stores;
+using osu.Framework.Platform;
+using osu.Framework.Testing;
+using osu.Game.Database;
+using osu.Game.IO;
+using osu.Game.Skinning;
+using osu.Game.Tests.Visual;
+using osuTK.Graphics;
+
+namespace osu.Plugin.LegacyBreakOverlay.Tests;
+
+public partial class TestSceneLegacyBreakOverlay : OsuTestScene, IStorageResourceProvider
+{
+    private LegacyBreakOverlay legacyBreakOverlay = null!;
+    private int animationLoopCount = 1;
+
+    public string LocalSkinPath
+        // the built-in legacy skin doesn't contiain required assets for this skin component
+        => throw new Exception("Set this to a valid local skin path to test with a specific skin.");
+
+    [SetUpSteps]
+    public virtual void SetUpSteps()
+    {
+        AddStep("Setup components", SetupComponents);
+
+        AddStep("Clear all animations", () => legacyBreakOverlay.ClearAllAnimations());
+        AddStep("Clear warning animation", () => legacyBreakOverlay.ClearWarningAnimation());
+
+        AddStep("Play passing animation", () => legacyBreakOverlay.PlayBreakRankingAnimation(true));
+        AddStep("Play failing animation", () => legacyBreakOverlay.PlayBreakRankingAnimation(false));
+
+        AddSliderStep("Animation loops", 1, 10, 2, v => animationLoopCount = v);
+        AddStep("Play warning animation", () => legacyBreakOverlay.PlayWarningAnimation(animationLoopCount));
+    }
+
+    protected virtual void SetupComponents()
+    {
+        var skin = new DefaultLegacySkin(this);
+
+        Children = new Drawable[]
+        {
+            new Box
+            {
+                Colour = Color4.Black,
+                RelativeSizeAxes = Axes.Both,
+            },
+            new SkinProvidingContainer(skin)
+            {
+                Child = legacyBreakOverlay = new LegacyBreakOverlay(),
+            }
+        };
+    }
+
+    private IResourceStore<byte[]> createResourceStore()
+        => new StorageBackedResourceStore(new NativeStorage(LocalSkinPath));
+
+    [Resolved]
+    private GameHost host { get; set; } = null!;
+
+    #region IResourceStorageProvider
+
+    public IRenderer Renderer => host.Renderer;
+    public AudioManager AudioManager => Audio;
+    public IResourceStore<byte[]> Files => Resources;
+    public new IResourceStore<byte[]> Resources => createResourceStore();
+    public IResourceStore<TextureUpload> CreateTextureLoaderStore(IResourceStore<byte[]> underlyingStore)
+        => host.CreateTextureLoaderStore(Resources);
+    RealmAccess IStorageResourceProvider.RealmAccess => null!;
+
+    #endregion
+}
---
+++ osu.Plugin.LegacyBreakOverlay/LegacyBreakOverlay.cs
@@ -0,0 +1,364 @@
+using System.Diagnostics;
+using osu.Framework.Allocation;
+using osu.Framework.Bindables;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Sprites;
+using osu.Framework.Graphics.Textures;
+using osu.Framework.Timing;
+using osu.Game.Audio;
+using osu.Game.Beatmaps;
+using osu.Game.Beatmaps.Timing;
+using osu.Game.Configuration;
+using osu.Game.Rulesets.Objects.Types;
+using osu.Game.Rulesets.Scoring;
+using osu.Game.Rulesets.UI;
+using osu.Game.Screens.Play;
+using osu.Game.Skinning;
+using osu.Game.Utils;
+using osuTK;
+
+namespace osu.Plugin.LegacyBreakOverlay;
+
+public partial class LegacyBreakOverlay : CompositeDrawable, ISerialisableDrawable
+{
+    bool ISerialisableDrawable.UsesFixedAnchor { get; set; } = true;
+
+    [SettingSource("Disable lazer break overlay", "Disables the built-in lazer break overlay.")]
+    public BindableBool DisableLazerBreakOverlay { get; } = new BindableBool(false);
+
+    [Resolved]
+    private Player? player { get; set; }
+
+    [Resolved]
+    private DrawableRuleset? drawableRuleset { get; set; }
+
+    [Resolved]
+    private ScoreProcessor? scoreProcessor { get; set; }
+
+    private Playfield? playfield => drawableRuleset?.Playfield;
+
+    [Resolved]
+    private ISkinSource? skin { get; set; } = null;
+
+    [Resolved]
+    private IBindable<WorkingBeatmap> beatmap { get; set; } = null!;
+
+    private BreakTracker? breakTracker;
+
+    private Container warningContainer = null!;
+    private Sprite sectionResultSprite;
+
+    private static readonly Vector2 warning_arrow_position = new Vector2(80, 100);
+    private const float warning_arrow_duration = 100;
+    public override IFrameBasedClock Clock => drawableRuleset is null ? base.Clock : drawableRuleset.FrameStableClock;
+
+    private BreakTracker? createBreakTracker() => drawableRuleset is null || scoreProcessor is null ? null : new BreakTracker(drawableRuleset.GameplayStartTime, scoreProcessor)
+    {
+        Breaks = beatmap.Value.Beatmap.Breaks
+    };
+
+    public LegacyBreakOverlay()
+    {
+        Anchor = Anchor.Centre;
+        Origin = Anchor.Centre;
+        Position = Vector2.Zero;
+        RelativeSizeAxes = Axes.Both;
+
+        InternalChildren = new Drawable[]
+        {
+            sectionResultSprite = new Sprite
+            {
+                Anchor = Anchor.Centre,
+                Origin = Anchor.Centre,
+            },
+            warningContainer = new Container()
+            {
+                Anchor = Anchor.Centre,
+                Origin = Anchor.Centre,
+                RelativeSizeAxes = Axes.Both,
+                Children = new Drawable[]
+                {
+                    new WarningArrow
+                    {
+                        Anchor = Anchor.TopLeft,
+                        Origin = Anchor.Centre,
+                        Position = warning_arrow_position,
+                    },
+                    new WarningArrow
+                    {
+                        Anchor = Anchor.BottomLeft,
+                        Origin = Anchor.Centre,
+                        Position = new Vector2(warning_arrow_position.X, -warning_arrow_position.Y),
+                    },
+                    new WarningArrow
+                    {
+                        Anchor = Anchor.TopRight,
+                        Origin = Anchor.Centre,
+                        Position = new Vector2(-warning_arrow_position.X, warning_arrow_position.Y),
+                        Scale = new Vector2(-1, 1),
+                    },
+                    new WarningArrow
+                    {
+                        Anchor = Anchor.BottomRight,
+                        Origin = Anchor.Centre,
+                        Position = new Vector2(-warning_arrow_position.X, -warning_arrow_position.Y),
+                        Scale = new Vector2(-1, 1),
+                    }
+                },
+            }
+        };
+    }
+
+    private PoolableSkinnableSample? sectionPassSample = null;
+    private PoolableSkinnableSample? sectionFailSample = null;
+
+    [BackgroundDependencyLoader]
+    private void load()
+    {
+        // FIXME: investigate this, samples from custom skins are not applying correctly.
+        AddInternal(sectionPassSample = new PoolableSkinnableSample(new SampleInfo("Gameplay/sectionpass")));
+        AddInternal(sectionFailSample = new PoolableSkinnableSample(new SampleInfo("Gameplay/sectionfail")));
+
+        preemptTimesForBreaks = calculatePreemptTimeForBreaks();
+    }
+
+    private double[] preemptTimesForBreaks = null!;
+
+    private double[] calculatePreemptTimeForBreaks()
+    {
+        var beatmap = this.beatmap.Value.Beatmap;
+        var breaks = beatmap.Breaks;
+
+        if (breaks.Count == 0)
+            return Array.Empty<double>();
+
+        var hitObjects = beatmap.HitObjects;
+        double[] preemptTimes = new double[breaks.Count];
+
+        for (int i = 0; i < preemptTimes.Length; i++)
+            preemptTimes[i] = double.NaN;
+
+        var currentBreak = 0;
+
+        for (int i = 0; i < hitObjects.Count; i++)
+        {
+            var hitObject = hitObjects[i];
+
+            if (hitObject.StartTime < breaks[currentBreak].StartTime)
+                continue;
+
+            double preemptTime;
+            if (hitObject is IHasTimePreempt hasTimePreempt)
+                preemptTime = hasTimePreempt.TimePreempt;
+            else
+                // FIXME
+                // I don't know what does this mean
+                // This line was from: https://github.com/ppy/osu/blob/e0c4592dc74a69aff1453a8e19e3ec0f5e8f2ca9/osu.Game/Screens/Edit/EditorBeatmapProcessor.cs#L88-L91
+                // i see this value is used when the next object is not IHasTimePreempt, so i guess it's a fallback value.
+                preemptTime = Math.Max(BreakPeriod.GAP_AFTER_BREAK, beatmap.ControlPointInfo.TimingPointAt(hitObject.StartTime).BeatLength * 2);
+
+            preemptTimes[currentBreak] = preemptTime;
+
+            if (++currentBreak >= breaks.Count)
+                break;
+        }
+
+        return preemptTimes;
+    }
+
+    protected override void LoadComplete()
+    {
+        base.LoadComplete();
+
+        DisableLazerBreakOverlay.BindValueChanged(v =>
+        {
+            var defaultBreakOverlay = player?.BreakOverlay;
+
+            if (defaultBreakOverlay == null)
+                return;
+
+            if (v.NewValue)
+                defaultBreakOverlay.Hide();
+            else
+                defaultBreakOverlay.Show();
+        }, true);
+
+        breakTracker = createBreakTracker();
+
+        if (breakTracker is not null)
+        {
+            AddInternal(breakTracker);
+
+            breakTracker.IsBreakTime.BindValueChanged(v =>
+            {
+                if (v.NewValue)
+                    playBreakAnimations();
+                else
+                    ClearAllAnimations();
+            });
+        }
+
+        ClearAllAnimations();
+    }
+
+    private bool isSectionPassing()
+    {
+        // lazer didn't provide sections passing/failing feedback, so we approximate it here.
+        if (scoreProcessor is null || breakTracker is null)
+            return true;
+
+        // This should be a reasonable approximation of passing a section.
+        return scoreProcessor.Accuracy.Value > 0.9;
+    }
+
+    private void playBreakAnimations()
+    {
+        Debug.Assert(breakTracker is not null);
+
+        PlayBreakRankingAnimation(isSectionPassing());
+
+        if (breakTracker.CurrentPeriod.Value is not Period currentBreak)
+            return;
+
+        double preemptTime = preemptTimesForBreaks.FirstOrDefault(b => b >= currentBreak.Start);
+
+        if (double.IsNaN(preemptTime))
+            return;
+
+        // stable uses int for these timings, we keep consistent.
+        int preemptCount = Math.Min(2, (int)(preemptTime / 200));
+        int flashCount = Math.Min(5, (int)((currentBreak.Duration + 200) / 200)) + preemptCount;
+        int loopStartTime = (int)(currentBreak.End - 200 * (flashCount - preemptCount));
+
+        using (BeginAbsoluteSequence(loopStartTime))
+            PlayWarningAnimation(flashCount);
+    }
+
+    public void PlayWarningAnimation(int loopCount)
+    {
+        if (loopCount <= 0)
+            return;
+
+        var transform = warningContainer.FadeIn()
+            .Delay(warning_arrow_duration)
+            .FadeOut();
+
+        if (loopCount > 1)
+            transform.Loop(warning_arrow_duration, loopCount);
+    }
+
+    public void ClearWarningAnimation()
+    {
+        warningContainer.ClearTransforms();
+        warningContainer.FadeOut();
+    }
+
+    public void PlayBreakRankingAnimation(bool passing)
+    {
+        double? beginTime = null;
+
+        if (breakTracker?.CurrentPeriod.Value is Period period)
+        {
+            double halfDuration = period.Duration / 2.0;
+
+            double gameStartTime = drawableRuleset?.GameplayStartTime ?? 0;
+            double playTime = (halfDuration > 2880) ? (period.Start + halfDuration) : (period.End - 2880);
+
+            beginTime = Math.Max(0, playTime - gameStartTime);
+        }
+
+        Texture? texture = skin?.GetTexture(passing ? "section-pass" : "section-fail");
+
+        if (texture is null)
+            return;
+
+        ClearBreakRankingAnimation();
+
+        sectionResultSprite.Texture = texture;
+
+        if (beginTime.HasValue)
+        {
+            using (BeginAbsoluteSequence(beginTime.Value))
+                playAnimation();
+
+            Scheduler.AddDelayed(playSample, beginTime.Value - Clock.CurrentTime);
+        }
+        else
+        {
+            playAnimation();
+            playSample();
+        }
+
+        void playSample()
+        {
+            if (passing)
+                sectionPassSample?.Play();
+            else
+                sectionFailSample?.Play();
+        }
+
+        void playAnimation()
+        {
+            if (passing)
+                palyPassingAnimation();
+            else
+                playFailingAnimation();
+        }
+    }
+
+    public void ClearAllAnimations()
+    {
+        ClearWarningAnimation();
+        ClearBreakRankingAnimation();
+    }
+
+    public void ClearBreakRankingAnimation()
+    {
+        sectionResultSprite.ClearTransforms();
+        sectionResultSprite.FadeOut();
+    }
+
+    private void palyPassingAnimation()
+    {
+        sectionResultSprite
+            .Delay(20)
+            .FadeInFromZero()
+            .Delay(80)
+            .FadeOutFromOne()
+            .Delay(60)
+            .FadeInFromZero()
+            .Delay(70)
+            .FadeOutFromOne()
+            .Delay(50)
+            .FadeInFromZero()
+            .Delay(1000)
+            .FadeOutFromOne(200);
+    }
+
+    private void playFailingAnimation()
+    {
+        sectionResultSprite
+            .Delay(130)
+            .FadeInFromZero()
+            .Delay(100)
+            .FadeOutFromOne()
+            .Delay(50)
+            .FadeInFromZero()
+            .Delay(1000)
+            .FadeOutFromOne(200);
+    }
+
+    private partial class WarningArrow : Sprite
+    {
+        [BackgroundDependencyLoader]
+        private void load(ISkinSource skin)
+        {
+            Texture? texture = skin.GetTexture("arrow-warning")
+                ?? skin.GetTexture("arrow-pause");
+
+            if (texture is not null)
+                Texture = texture;
+        }
+    }
+}
