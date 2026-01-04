7f0e52dc8436469e87e41a6706eec92fd4fa6304
Caiyi Hsu <Cai1Hsu@outlook.com>
2026-01-04 16:22:23 +0800

    fix: update GameName reference in tests to use TestOsuGame constant

--- osu.Game.Rulesets.PluginsLoader.Tests/Program.cs
+++ osu.Game.Rulesets.PluginsLoader.Tests/Program.cs
@@ -1,9 +1,8 @@
 ﻿using osu.Framework;
+using osu.Game.Rulesets.PluginsLoader.Tests;
 using osu.Game.Tests;
 
-const string GameName = @"osu-development";
-
-using (var host = Host.GetSuitableDesktopHost(GameName, new HostOptions()))
+using (var host = Host.GetSuitableDesktopHost(TestOsuGame.GameName, new HostOptions()))
 {
     host.Run(new OsuTestBrowser());
 }
--- osu.Game.Rulesets.PluginsLoader.Tests/TestOsuGame.cs
+++ osu.Game.Rulesets.PluginsLoader.Tests/TestOsuGame.cs
@@ -1,7 +1,11 @@
+using osu.Framework.Platform;
+
 namespace osu.Game.Rulesets.PluginsLoader.Tests;
 
 public partial class TestOsuGame : OsuGame
 {
+    public const string GameName = @"osu-development";
+
     public override bool UseDevelopmentServer => true;
 
     public override Version AssemblyVersion => new Version(0, 0, 0, 0);
