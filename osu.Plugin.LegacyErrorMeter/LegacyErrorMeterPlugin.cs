458198374f0d24f33724efe5071ef8e276e40f0a
Caiyi Hsu <Cai1Hsu@outlook.com>
2026-01-05 01:00:06 +0800

    feat: integrate SkinEditorOverlay and register LegacyErrorMeter components

--- osu.Plugin.LegacyErrorMeter/LegacyErrorMeterPlugin.cs
+++ osu.Plugin.LegacyErrorMeter/LegacyErrorMeterPlugin.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using osu.Framework.Allocation;
 using osu.Framework.Threading;
 using osu.Game;
+using osu.Game.Overlays.SkinEditor;
 using osu.Game.Plugins;
+using osu.Game.Plugins.Skins;
+using osu.Game.Skinning;
 
 namespace osu.Plugin.LegacyErrorMeter;
 
@@ -9,5 +12,15 @@ public class LegacyErrorMeterPlugin : OsuPlugin
 {
     public override void OnLoad(OsuGameBase gameBase, Scheduler scheduler)
     {
+        var game = (OsuGame)gameBase;
+        SkinEditorOverlay? skinEditor = game.Dependencies.Get<SkinEditorOverlay?>();
+
+        if (skinEditor is null)
+            return;
+
+        skinEditor.RegisterSkinComponents(new[]
+        {
+            typeof(LegacyErrorMeter)
+        }, new GlobalSkinnableContainerLookup(GlobalSkinnableContainers.MainHUDComponents, null));
     }
 }
