2578829754edea1b3b2db6226a9955d467c38e9b
Caiyi Hsu <Cai1Hsu@outlook.com>
2026-01-06 14:27:05 +0800

    feat: add legacy resource management and associated assets

---
+++ osu.Game.Plugins.Legacy/LegacyResources.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+
+namespace osu.Game.Plugins.Legacy;
+
+public static class LegacyResources
+{
+    public static Assembly ResourceAssembly => typeof(LegacyResources).Assembly;
+}
