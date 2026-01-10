ffa30edab8c969347c24203daa45f19de7e71678
Caiyi Hsu <Cai1Hsu@outlook.com>
2026-01-11 01:24:08 +0800

    allow using `LegacySpriteText` without `LegacySpriteTextContainer`

---
+++ osu.Game.Plugins.Legacy/LegacySpriteText.cs
@@ -0,0 +1,158 @@
+// This file is adapted from osu!lazer's LegacySpriteText to support more legacy skin fonts.
+// Original file: https://github.com/ppy/osu/blob/master/osu.Game/Skinning/LegacySpriteText.cs
+
+using System.Collections.Frozen;
+using osu.Framework.Allocation;
+using osu.Framework.Graphics.Sprites;
+using osu.Framework.Graphics.Textures;
+using osu.Framework.Text;
+using osu.Game.Graphics.Sprites;
+using osu.Game.Skinning;
+using osuTK;
+
+namespace osu.Game.Plugins.Legacy;
+
+/// <summary>
+/// A sprite text which uses legacy osu! font textures.
+/// </summary>
+public partial class LegacySpriteText : OsuSpriteText
+{
+    public Vector2? MaxSizePerGlyph { get; set; }
+    public bool FixedWidth { get; set; }
+
+    public delegate Texture? TextureLookupDelegate(string lookupName);
+
+    private ITexturedGlyphLookupStore glyphStore = null!;
+
+    private readonly string fontPrefix;
+
+    public TextureLookupDelegate? TextureLookup { get; set; }
+
+    public FrozenDictionary<char, string>? CustomMappings { get; set; }
+
+    public LegacySpriteText(string fontPrefix)
+    {
+        this.fontPrefix = fontPrefix;
+    }
+
+    [BackgroundDependencyLoader]
+    private void load()
+    {
+        glyphStore ??= new LegacyGlyphStore(fontPrefix, TextureLookup!, MaxSizePerGlyph, CustomMappings);
+        base.Font = new FontUsage(fontPrefix, 1, fixedWidth: FixedWidth);
+
+        // cache common lookups ahead of time.
+        foreach (char c in FixedWidthExclude)
+            glyphStore.Get(fontPrefix, c);
+        for (int i = 0; i < 10; i++)
+            glyphStore.Get(fontPrefix, (char)('0' + i));
+    }
+
+    protected virtual TextureLookupDelegate CreateTextureLookup(IReadOnlyDependencyContainer dependencies)
+    {
+        var skin = dependencies.Get<ISkinSource>()
+            ?? throw new InvalidOperationException($"No {nameof(ISkinSource)} available in the dependency container.");
+        return skin.GetTexture;
+    }
+
+    protected override TextBuilder CreateTextBuilder(ITexturedGlyphLookupStore _)
+        => base.CreateTextBuilder(glyphStore);
+
+    private readonly static char[] defaultFixedWidthExcludeCharacters = new[] { ',', '.', '%', 'x', '/' };
+
+    private char fixedWidthReferenceCharacter = '0';
+    private char[] fixedWidthExcludeCharacters = defaultFixedWidthExcludeCharacters;
+
+    protected override char FixedWidthReferenceCharacter => FixedWidthReference;
+    protected override char[] FixedWidthExcludeCharacters => FixedWidthExclude;
+
+    public char FixedWidthReference
+    {
+        get => fixedWidthReferenceCharacter;
+        set => fixedWidthReferenceCharacter = value;
+    }
+
+    public char[] FixedWidthExclude
+    {
+        get => fixedWidthExcludeCharacters;
+        set => fixedWidthExcludeCharacters = value;
+    }
+
+    public float FontOverlap
+    {
+        get => -Spacing.X;
+        set => Spacing = new Vector2(-value, Spacing.Y);
+    }
+
+    internal class LegacyGlyphStore : ITexturedGlyphLookupStore
+    {
+        private readonly TextureLookupDelegate textureLookup;
+        private readonly Vector2? maxSize;
+
+        private readonly string fontName;
+
+        private readonly Dictionary<char, ITexturedCharacterGlyph?> cache = new Dictionary<char, ITexturedCharacterGlyph?>();
+
+        private FrozenDictionary<char, string>? customMappings;
+
+        public LegacyGlyphStore(string fontName, TextureLookupDelegate textureLookup, Vector2? maxSize, FrozenDictionary<char, string>? customMappings = null)
+        {
+            this.fontName = fontName;
+            this.textureLookup = textureLookup;
+            this.maxSize = maxSize;
+            this.customMappings = customMappings;
+
+            ArgumentNullException.ThrowIfNull(textureLookup);
+        }
+
+        public ITexturedCharacterGlyph? Get(string? fontName, char character)
+        {
+            // We only service one font.
+            if (fontName != this.fontName)
+                return null;
+
+            if (cache.TryGetValue(character, out var cached))
+                return cached;
+
+            string lookup = getLookupName(character);
+
+            var texture = textureLookup($"{fontName}-{lookup}");
+
+            TexturedCharacterGlyph? glyph = null;
+
+            if (texture != null)
+            {
+                if (maxSize != null)
+                    texture = texture.WithMaximumSize(maxSize.Value);
+
+                glyph = new TexturedCharacterGlyph(new CharacterGlyph(character, 0, 0, texture.Width, 0, null), texture, 1f / texture.ScaleAdjust);
+            }
+
+            cache[character] = glyph;
+            return glyph;
+        }
+
+        private string getLookupName(char character)
+        {
+            if (customMappings != null && customMappings.TryGetValue(character, out var mapping))
+                return mapping;
+
+            switch (character)
+            {
+                case ',':
+                    return "comma";
+
+                case '.':
+                    return "dot";
+
+                case '%':
+                    return "percent";
+
+                default:
+                    return character.ToString();
+            }
+        }
+
+        public Task<ITexturedCharacterGlyph?> GetAsync(string fontName, char character) => Task.Run(() => Get(fontName, character));
+    }
+}
