1a4d412def274c679fc0a3c5cdc871ffeb06767d
Caiyi Hsu <Cai1Hsu@outlook.com>
2026-01-11 19:27:23 +0800

    fix LegacyLeaderboardEntry test scene

--- osu.Plugin.LegacyLeaderboard.Tests/TestSceneLegacyLeaderboardEntry.cs
+++ osu.Plugin.LegacyLeaderboard.Tests/TestSceneLegacyLeaderboardEntry.cs
@@ -1,5 +1,6 @@
 using osu.Framework.Allocation;
 using osu.Framework.Audio;
+using osu.Framework.Bindables;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Rendering;
 using osu.Framework.Graphics.Textures;
@@ -8,16 +9,45 @@ using osu.Framework.Platform;
 using osu.Framework.Testing;
 using osu.Game.Database;
 using osu.Game.IO;
-using osu.Game.Scoring;
-using osu.Game.Screens.Select.Leaderboards;
+using osu.Game.Online.API;
+using osu.Game.Online.API.Requests.Responses;
 using osu.Game.Skinning;
 using osu.Game.Tests.Visual;
-using osuTK;
 
 namespace osu.Plugin.LegacyLeaderboard.Tests;
 
 public partial class TestSceneLegacyLeaderboardEntry : OsuTestScene, IStorageResourceProvider
 {
+    private DummyAPIAccess api => (DummyAPIAccess)API;
+
+    private APIUser friend = new APIUser
+    {
+        Id = 2,
+        Username = "Friend",
+    };
+
+    private APIUser localUser => api.LocalUser.Value;
+
+    private readonly Bindable<int?> ScorePosition = new Bindable<int?>();
+
+    private readonly BindableBool HasQuit = new BindableBool();
+
+    private readonly BindableLong TotalScore = new BindableLong();
+
+    private readonly BindableInt Combo = new BindableInt();
+
+    private LegacyLeaderboardEntry entry = null!;
+
+    [BackgroundDependencyLoader]
+    private void load()
+    {
+        api.LocalUserState.Friends.Add(new APIRelation()
+        {
+            TargetUser = friend,
+            TargetID = friend.Id,
+        });
+    }
+
     [SetUpSteps]
     public void SetUpSteps()
     {
@@ -29,14 +59,49 @@ public partial class TestSceneLegacyLeaderboardEntry : OsuTestScene, IStorageRes
             {
                 Anchor = Anchor.Centre,
                 Origin = Anchor.Centre,
-                // FIXME: test broken due to dependency of leaderboard and score info.
-                Child = new LegacyLeaderboardEntry(new GameplayLeaderboardScore(new ScoreInfo(), true, GameplayLeaderboardScore.ComboDisplayMode.Current))
+                Child = entry = new LegacyLeaderboardEntry()
                 {
                     Anchor = Anchor.Centre,
                     Origin = Anchor.Centre,
+                    ScorePosition = { BindTarget = ScorePosition },
+                    HasQuit = { BindTarget = HasQuit },
+                    TotalScore = { BindTarget = TotalScore },
+                    Combo = { BindTarget = Combo },
                 }
             };
         });
+
+        AddStep("Set local user", () => updateUser(localUser));
+
+        AddStep("Clear position", () => ScorePosition.Value = null);
+        AddSliderStep("Set position", 1, 100, 50, pos => ScorePosition.Value = (int?)pos);
+
+        AddToggleStep("Set quit", value => HasQuit.Value = value);
+        AddSliderStep("Set score", 0, 1_000_000, 500_000, score => TotalScore.Value = score);
+        AddSliderStep("Set combo", 0, 2000, 500, combo => Combo.Value = combo);
+
+        AddStep("Set friend user", () => updateUser(friend));
+        AddStep("Clear user", () => updateUser(null!));
+
+        AddToggleStep("Set tracking", updateTracking);
+    }
+
+    private void updateTracking(bool isTracking)
+    {
+        if (entry is null)
+            return;
+
+        entry.IsTracking = isTracking;
+        entry.UpdatePanelState();
+    }
+
+    private void updateUser(APIUser user)
+    {
+        if (entry is null)
+            return;
+
+        entry.User = user;
+        entry.UpdatePanelState();
     }
 
     [Resolved]
