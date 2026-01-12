8d3fc08070aa7376102723c8452b5c9e264823a2
Caiyi Hsu <Cai1Hsu@outlook.com>
2026-01-12 22:49:22 +0800

    fix: update host name in test program to use 'osu-development'

--- osu.Plugin.LegacyExperience.Tests/Program.cs
+++ osu.Plugin.LegacyExperience.Tests/Program.cs
@@ -2,7 +2,7 @@ using osu.Framework;
 using osu.Plugin.LegacyExperience;
 using osu.Game.Tests;
 
-using (var host = Host.GetSuitableDesktopHost("osu", new HostOptions()))
+using (var host = Host.GetSuitableDesktopHost("osu-development", new HostOptions()))
 {
     var game = new OsuTestBrowser();
     game.EnsureLegacyResources();
