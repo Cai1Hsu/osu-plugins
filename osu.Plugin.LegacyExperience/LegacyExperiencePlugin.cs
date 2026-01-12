0092de02b00b91f7954d26bae6dbaabcd28d920a
Caiyi Hsu <Cai1Hsu@outlook.com>
2026-01-12 22:59:15 +0800

    feat: add LegacyExperiencePlugin with skin editor integration

---
+++ osu.Plugin.LegacyExperience/LegacyExperiencePlugin.cs
@@ -0,0 +1,53 @@
+using osu.Framework.Allocation;
+using osu.Framework.Threading;
+using osu.Game;
+using osu.Game.Overlays.SkinEditor;
+using osu.Game.Plugins;
+using osu.Game.Plugins.Skins;
+using osu.Game.Rulesets;
+using osu.Game.Skinning;
+using osu.Plugin.LegacyExperience.Buttons;
+using osu.Plugin.LegacyExperience.Gameplay;
+using osu.Plugin.LegacyExperience.Leaderboards;
+
+namespace osu.Plugin.LegacyExperience;
+
+public sealed class LegacyExperiencePlugin : OsuPlugin
+{
+    public override void OnLoad(OsuGameBase gameBase, Scheduler scheduler)
+    {
+        var game = (OsuGame)gameBase;
+
+        SkinEditorOverlay? skinEditor = game.Dependencies.Get<SkinEditorOverlay?>();
+        if (skinEditor is null)
+            return;
+
+        var osuRuleset = new RulesetInfo
+        {
+            ShortName = "osu",
+            OnlineID = 0,
+            Available = true,
+        };
+
+        skinEditor.RegisterSkinComponents(new[]
+        {
+            typeof(LegacyBreakOverlay),
+            // In stable, only osu!standard has the break overlay.
+        }, new GlobalSkinnableContainerLookup(GlobalSkinnableContainers.MainHUDComponents, osuRuleset));
+
+        skinEditor.RegisterSkinComponents(new[]
+        {
+            typeof(LegacyHealthOverlay),
+            typeof(LegacyComboCounter),
+            typeof(LegacyErrorMeter),
+            typeof(LegacyFpsDisplay),
+            typeof(LegacyLeaderboard),
+        }, new GlobalSkinnableContainerLookup(GlobalSkinnableContainers.MainHUDComponents));
+
+        skinEditor.RegisterSkinComponents(new[]
+        {
+            typeof(LegacyChatButton),
+            typeof(LegacyDashboardButton),
+        }, new GlobalSkinnableContainerLookup(GlobalSkinnableContainers.SongSelect));
+    }
+}
