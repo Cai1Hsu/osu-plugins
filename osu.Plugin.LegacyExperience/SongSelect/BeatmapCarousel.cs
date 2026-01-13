559ce09589a98a0dd2db72ad983e40ef7d84ec2e
Caiyi Hsu <Cai1Hsu@outlook.com>
2026-01-13 16:41:29 +0800

    add basic legacy beatmap panel and carousel implementation

---
+++ osu.Plugin.LegacyExperience/SongSelect/BeatmapCarousel.cs
@@ -0,0 +1,146 @@
+using System.Runtime.CompilerServices;
+using osu.Framework.Allocation;
+using osu.Framework.Caching;
+using osu.Framework.Graphics;
+using osu.Game.Beatmaps;
+using osu.Game.Graphics.Carousel;
+using osu.Game.Skinning;
+using BeatmapCarouselV2 = osu.Game.Screens.SelectV2.BeatmapCarousel;
+
+namespace osu.Plugin.LegacyExperience.SongSelect;
+
+public partial class BeatmapCarousel : BeatmapCarouselV2
+{
+    [Resolved]
+    private ISkinSource? skin { get; set; }
+
+    protected override void LoadComplete()
+    {
+        base.LoadComplete();
+
+        if (skin is not null)
+        {
+            skin.SourceChanged += onSkinSourceChanged;
+
+            onSkinSourceChanged();
+        }
+    }
+
+    private float panelHeight = 103 * 0.6f;
+
+    private void onSkinSourceChanged()
+    {
+        var backgroundTexture = skin?.GetTexture("menu-button-background");
+
+        if (backgroundTexture is null)
+            return;
+
+        panelHeight = backgroundTexture.DisplayHeight * LegacyPanel.TextureScale;
+
+        var filterAfterItemsChanged = get_filter_after_items_changed(this);
+        filterAfterItemsChanged.Invalidate();
+    }
+
+    // FIXME: POC stage temporarily uses unsafe accessor
+    // use reflection as some deploy platforms do not support UnsafeAccessor
+    [UnsafeAccessor(UnsafeAccessorKind.Field, Name = "filterAfterItemsChanged")]
+    private static extern ref Cached get_filter_after_items_changed(Carousel<BeatmapInfo> carousel);
+
+    private const float hover_expand_amount_y = 10;
+
+    private const float hover_expand_amount_x = 50;
+
+    protected override void Update()
+    {
+        base.Update();
+
+        var scrollChildren = Scroll.Panels.Children;
+
+        double? hoveredY = null;
+
+        for (int i = 0; i < scrollChildren.Count; i++)
+        {
+            var child = scrollChildren[i];
+            if (child is not LegacyPanel panel)
+                continue;
+
+            if (panel.IsHovered && panel.Item is CarouselItem item)
+                hoveredY = item.CarouselYPosition;
+        }
+
+        // expand the hovered panel and push others away
+        double frameRatio = Time.Elapsed / (1000 / 60f);
+        double dampingFactor = Math.Pow(0.875, frameRatio);
+
+        // bypass Carousel's Y position damping
+        for (int i = 0; i < scrollChildren.Count; i++)
+        {
+            var child = scrollChildren[i];
+
+            if (child is not LegacyPanel panel)
+                continue;
+
+            if (panel.Item is not CarouselItem item)
+                continue;
+
+            double targetY = item.CarouselYPosition;
+
+            if (hoveredY.HasValue && !panel.IsHovered)
+            {
+                if (targetY > hoveredY.Value)
+                    targetY += hover_expand_amount_y;
+                else if (targetY < hoveredY.Value)
+                    targetY -= hover_expand_amount_y;
+            }
+
+            double currentY = panel.OsuDrawYPosition == item.CarouselYPosition
+                ? panel.OsuDrawYPosition // newly added panels
+                : panel.DrawYPosition; // our managed Y position, used to bypass Carousel's damping
+
+            double offsetY = targetY - currentY;
+            offsetY *= dampingFactor;
+
+            panel.OsuDrawYPosition = targetY - offsetY;
+            panel.DrawYPosition = panel.OsuDrawYPosition;
+        }
+    }
+
+    protected override float GetSpacingBetweenPanels(CarouselItem previousVisible, CarouselItem bottom)
+        => 0; // seems good enough, maybe reference for stable later
+
+    protected override async Task<IEnumerable<CarouselItem>> FilterAsync(bool clearExistingPanels = false)
+    {
+        var items = await base.FilterAsync(clearExistingPanels);
+
+        foreach (var item in items)
+            item.DrawHeight = panelHeight;
+
+        return items;
+    }
+
+    protected override float GetPanelXOffset(Drawable panel)
+    {
+        var xPosition = base.GetPanelXOffset(panel);
+
+        if (panel is LegacyPanel legacyPanel)
+        {
+            if (panel.IsHovered)
+                xPosition -= hover_expand_amount_x;
+
+            float currentX = legacyPanel.X;
+            double frameRatio = Time.Elapsed / (1000 / 60f);
+            float offsetX = xPosition - currentX;
+            offsetX *= (float)Math.Pow(0.95, frameRatio);
+
+            xPosition -= offsetX;
+        }
+
+        return xPosition;
+    }
+
+    protected override Drawable GetDrawableForDisplay(CarouselItem item)
+    {
+        // TODO: pool and select corresponding LegacyPanel type based on item's model
+        return new LegacyPanel();
+    }
+}
---
+++ osu.Plugin.LegacyExperience/SongSelect/LegacyPanel.cs
@@ -0,0 +1,74 @@
+using osu.Framework.Graphics;
+using osu.Framework.Allocation;
+using osu.Framework.Bindables;
+using osu.Framework.Graphics.Pooling;
+using osu.Game.Graphics.Carousel;
+using osu.Game.Skinning;
+using osuTK;
+using osu.Framework.Graphics.Sprites;
+
+namespace osu.Plugin.LegacyExperience.SongSelect;
+
+public partial class LegacyPanel : PoolableDrawable, ICarouselPanel
+{
+    internal const float TextureScale = 0.6f * 1.6f;
+
+    public BindableBool Selected { get; private set; } = new BindableBool();
+
+    public BindableBool Expanded { get; private set; } = new BindableBool();
+
+    public BindableBool KeyboardSelected { get; private set; } = new BindableBool();
+
+    // Legacy carousel managed, used for bypass Carousel's damping
+    public double DrawYPosition { get; set; }
+
+    public double OsuDrawYPosition
+    {
+        get => ((ICarouselPanel)this).DrawYPosition;
+        set => ((ICarouselPanel)this).DrawYPosition = value;
+    }
+
+    double ICarouselPanel.DrawYPosition { get; set; }
+    public CarouselItem? Item { get; set; }
+
+    public override bool HandlePositionalInput => true;
+
+    [BackgroundDependencyLoader]
+    private void load(ISkinSource? skin)
+    {
+        AutoSizeAxes = Axes.Both;
+
+        Sprite backgroundSprite;
+
+        AddInternal(backgroundSprite = new Sprite()
+        {
+            Anchor = Anchor.CentreLeft,
+            Origin = Anchor.CentreLeft,
+            Scale = new Vector2(TextureScale),
+        });
+
+        if (skin is not null)
+        {
+            skin.SourceChanged += updateTexture;
+            updateTexture();
+        }
+
+        void updateTexture()
+        {
+            // TODO: Song select requires dynamic textures loading when skin changes
+            // SkinnableSprite doestn't scale with @2x, so we manually retrieve the texture here.
+            // This is a temporary workaround to make size correct.
+            var texture = skin?.GetTexture("menu-button-background");
+
+            if (texture is null)
+                return;
+
+            backgroundSprite.Texture = texture;
+            backgroundSprite.Size = texture.DisplaySize;
+        }
+    }
+
+    public virtual void Activated()
+    {
+    }
+}
