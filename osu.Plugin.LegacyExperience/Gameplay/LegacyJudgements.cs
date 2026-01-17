b6213e62f572a567b74a47c19272095518cca154
Caiyi Hsu <Cai1Hsu@outlook.com>
2026-01-17 23:59:03 +0800

    add basic LegacyJudgements and associated shaders

---
+++ osu.Plugin.LegacyExperience/Gameplay/LegacyJudgements.cs
@@ -0,0 +1,227 @@
+using System.Runtime.InteropServices;
+using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Primitives;
+using osu.Framework.Graphics.Rendering;
+using osu.Framework.Graphics.Rendering.Vertices;
+using osu.Framework.Graphics.Shaders;
+using osu.Framework.Graphics.Shaders.Types;
+using osuTK.Graphics.ES30;
+using osuTK;
+
+namespace osu.Plugin.LegacyExperience.Gameplay;
+
+public partial class LegacyJudgements : Drawable
+{
+    private const int max_sparks = 1024;
+    private const long unused_invalidation_id = -1;
+    private const float initial_spark_alpha = 0.4f;
+
+    private readonly JudgementSpark[] sparks = new JudgementSpark[max_sparks];
+    private int sparkIndex = 0;
+
+    private float sparkLifetime = 10000; // 10s
+    private Vector2 sparkSize = new Vector2(3, 10); // TODO: adjust it.
+    private float time = 0;
+
+    public bool EnableClipping { get; set; } = true;
+
+    private IShader shader = null!;
+
+    [BackgroundDependencyLoader]
+    private void load(ShaderManager shaders)
+    {
+        shader = shaders.Load(@"LegacyJudgements", "LegacyJudgements");
+
+        for (int i = 0; i < max_sparks; i++)
+            sparks[i].InvalidationId = unused_invalidation_id;
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+
+        time = (float)Time.Current;
+
+        Invalidate(Invalidation.DrawNode);
+    }
+
+    public void SpawnSpark(Vector2 position, Colour4 color)
+    {
+        sparks[sparkIndex].Position = position;
+        sparks[sparkIndex].Color = color;
+        sparks[sparkIndex].Time = time;
+        ++sparks[sparkIndex].InvalidationId;
+
+        sparkIndex = (sparkIndex + 1) % max_sparks;
+    }
+
+    protected override DrawNode CreateDrawNode() => new LegacyJudgementsDrawNode(this);
+
+    private struct JudgementSpark
+    {
+        public Vector2 Position;
+        public Colour4 Color;
+        public float Time;
+        public long InvalidationId;
+    }
+
+    partial class LegacyJudgementsDrawNode : DrawNode
+    {
+        private new LegacyJudgements Source { get; set; }
+        private readonly JudgementSpark[] sparks = new JudgementSpark[max_sparks];
+
+        public LegacyJudgementsDrawNode(LegacyJudgements source) : base(source)
+        {
+            Source = source;
+        }
+
+        private IShader shader = null!;
+        private float time;
+        private float sparkLifetime;
+        private Vector2 sparkSize;
+        private bool enableClipping;
+        private RectangleF drawRectangle;
+
+        public override void ApplyState()
+        {
+            base.ApplyState();
+
+            shader = Source.shader;
+            time = Source.time;
+            sparkLifetime = Source.sparkLifetime;
+            sparkSize = Source.sparkSize;
+            enableClipping = Source.EnableClipping;
+            drawRectangle = Source.DrawRectangle;
+
+            Source.sparks.CopyTo(sparks, 0);
+        }
+
+        private IVertexBatch<JudgementSparkVertex> vertexBatch = null!;
+        private IUniformBuffer<JudgementsParameters> parametersBuffer = null!;
+
+        protected override void Draw(IRenderer renderer)
+        {
+            base.Draw(renderer);
+
+            // no need to draw anything. also prevent divide by zero in shader.
+            if (sparkLifetime <= 0)
+                return;
+
+            vertexBatch ??= renderer.CreateQuadBatch<JudgementSparkVertex>(max_sparks, 1);
+            parametersBuffer ??= renderer.CreateUniformBuffer<JudgementsParameters>();
+            parametersBuffer.Data = parametersBuffer.Data with
+            {
+                InitialAlpha = initial_spark_alpha,
+                Time = time,
+                SparkLifetime = sparkLifetime,
+            };
+
+            shader.Bind();
+            shader.BindUniformBlock(@"m_JudgementsParameters", parametersBuffer);
+
+            renderer.SetBlend(BlendingParameters.Additive);
+            renderer.PushLocalMatrix(DrawInfo.Matrix);
+
+            Vector2 halfSparkSize = sparkSize / 2;
+
+            foreach (var spark in sparks)
+            {
+                if (spark.InvalidationId == unused_invalidation_id)
+                    continue;
+
+                if (spark.Time + sparkLifetime < time)
+                    continue;
+
+                // bottom left
+                vertexBatch.Add(new JudgementSparkVertex
+                {
+                    Position = restrictToDrawRectangle(
+                        new Vector2(spark.Position.X - halfSparkSize.X, spark.Position.Y + halfSparkSize.Y)),
+                    Color = spark.Color * DrawColourInfo.Colour.BottomLeft.Linear,
+                    Time = spark.Time
+                });
+
+                // bottom right
+                vertexBatch.Add(new JudgementSparkVertex
+                {
+                    Position = restrictToDrawRectangle(
+                        new Vector2(spark.Position.X + halfSparkSize.X, spark.Position.Y + halfSparkSize.Y)),
+                    Color = spark.Color * DrawColourInfo.Colour.BottomRight.Linear,
+                    Time = spark.Time
+                });
+
+                // top right
+                vertexBatch.Add(new JudgementSparkVertex
+                {
+                    Position = restrictToDrawRectangle(
+                        new Vector2(spark.Position.X + halfSparkSize.X, spark.Position.Y - halfSparkSize.Y)),
+                    Color = spark.Color * DrawColourInfo.Colour.TopRight.Linear,
+                    Time = spark.Time
+                });
+
+                // top left
+                vertexBatch.Add(new JudgementSparkVertex
+                {
+                    Position = restrictToDrawRectangle(
+                        new Vector2(spark.Position.X - halfSparkSize.X, spark.Position.Y - halfSparkSize.Y)),
+                    Color = spark.Color * DrawColourInfo.Colour.TopLeft.Linear,
+                    Time = spark.Time
+                });
+            }
+
+            renderer.PopLocalMatrix();
+
+            vertexBatch.Draw();
+            shader.Unbind();
+        }
+
+        private Vector2 restrictToDrawRectangle(Vector2 position)
+        {
+            if (!enableClipping)
+                return position;
+
+            return new Vector2(
+                MathHelper.Clamp(position.X, drawRectangle.Left, drawRectangle.Right),
+                MathHelper.Clamp(position.Y, drawRectangle.Top, drawRectangle.Bottom)
+            );
+        }
+
+        protected override void Dispose(bool isDisposing)
+        {
+            base.Dispose(isDisposing);
+
+            vertexBatch?.Dispose();
+            parametersBuffer?.Dispose();
+        }
+
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        private record struct JudgementsParameters
+        {
+            public UniformFloat InitialAlpha;
+            public UniformFloat Time;
+            public UniformFloat SparkLifetime;
+            private readonly UniformPadding4 padding;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        struct JudgementSparkVertex : IVertex, IEquatable<JudgementSparkVertex>
+        {
+            [VertexMember(2, VertexAttribPointerType.Float)]
+            public Vector2 Position;
+
+            [VertexMember(4, VertexAttribPointerType.Float)]
+            public Colour4 Color;
+
+            [VertexMember(1, VertexAttribPointerType.Float)]
+            public float Time;
+
+            public bool Equals(JudgementSparkVertex other)
+            {
+                return Position.Equals(other.Position) &&
+                    Color.Equals(other.Color) &&
+                    Time.Equals(other.Time);
+            }
+        }
+    }
+}
---
+++ osu.Plugin.LegacyExperience/Shaders/sh_LegacyJudgements.vs
@@ -0,0 +1,28 @@
+#ifndef LEGACYJUDGEMENTS_VS
+#define LEGACYJUDGEMENTS_VS
+
+layout(location = 0) in vec2 m_Position;
+layout(location = 1) in vec4 m_Color;
+layout(location = 2) in float m_Time;
+
+layout(location = 0) out vec4 v_Color;
+
+layout(std140, set = 0, binding = 0) uniform m_JudgementsParameters
+{
+    float g_InitialAlpha;
+    float g_Time;
+    float g_SparkLifetime;
+};
+
+void main(void)
+{
+    float elapsed = g_Time - m_Time;
+    float progress = clamp(elapsed / g_SparkLifetime, 0.0, 1.0);
+
+    float alpha = g_InitialAlpha * (1.0 - progress);
+
+    v_Color = vec4(m_Color.xyz, alpha);
+    gl_Position = g_ProjMatrix * vec4(m_Position, 1.0, 1.0);
+}
+
+#endif
