c36d4a65126a6fae730db0c4fdb79d7378b85190
Caiyi Hsu <Cai1Hsu@outlook.com>
2026-01-17 23:59:14 +0800

    feat: add TestSceneLegacyJudgements for legacy judgements interaction testing

---
+++ osu.Plugin.LegacyExperience.Tests/Gameplay/TestSceneLegacyJudgements.cs
@@ -0,0 +1,63 @@
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Shapes;
+using osu.Framework.Input;
+using osu.Framework.Testing;
+using osu.Game.Tests.Visual;
+using osu.Plugin.LegacyExperience.Gameplay;
+using osuTK;
+
+namespace osu.Plugin.LegacyExperience.Tests.Gameplay;
+
+public partial class TestSceneLegacyJudgements : OsuTestScene
+{
+    private LegacyJudgements legacyJudgements = null!;
+
+    private InputManager inputManager = null!;
+
+    protected override void LoadComplete()
+    {
+        base.LoadComplete();
+
+        inputManager = GetContainingInputManager();
+    }
+
+    [SetUpSteps]
+    public void SetupSteps()
+    {
+        AddStep("add legacy judgements", () =>
+        {
+            Add(new ClickableContainer()
+            {
+                Anchor = Anchor.Centre,
+                Origin = Anchor.Centre,
+                Size = new Vector2(100, 10),
+                Scale = new Vector2(5),
+                Children = new Drawable[]
+                {
+                    new Box
+                    {
+                        Anchor = Anchor.Centre,
+                        Origin = Anchor.Centre,
+                        RelativeSizeAxes = Axes.Both,
+                        Colour = Colour4.DarkGray.Opacity(0.5f),
+                    },
+                    legacyJudgements = new LegacyJudgements
+                    {
+                        Anchor = Anchor.Centre,
+                        Origin = Anchor.Centre,
+                        RelativeSizeAxes = Axes.Both,
+                    }
+                },
+                Action = () =>
+                {
+                    if (legacyJudgements is null)
+                        return;
+
+                    var spawnPosition = legacyJudgements.ToLocalSpace(inputManager.CurrentState.Mouse.Position);
+                    legacyJudgements.SpawnSpark(spawnPosition, Colour4.Red);
+                }
+            });
+        });
+    }
+}
