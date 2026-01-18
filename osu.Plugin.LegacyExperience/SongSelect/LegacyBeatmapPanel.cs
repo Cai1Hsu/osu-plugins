a2833cbe40a1b9f53f3971f59b1cbfec53add2ff
Caiyi Hsu <Cai1Hsu@outlook.com>
2026-01-19 02:01:25 +0800

    fix: improve local rank display update logic for better user experience

--- osu.Plugin.LegacyExperience/SongSelect/LegacyBeatmapPanel.cs
+++ osu.Plugin.LegacyExperience/SongSelect/LegacyBeatmapPanel.cs
@@ -219,11 +219,27 @@ public partial class LegacyBeatmapPanel : LegacyPanel
             addStarDifficultyDisplay();
             computeStarRating(playBeatmap);
 
-            // Realm notification registration is VERY expensive and can NOT be done asynchronously.
-            // Generally we would cache the local scores for all beatmaps beforehand,
-            // but since we are just patching lazer's code instead of rewriting the whole song select, 
-            // we will just debounce the updates to reduce the number of registrations.
-            local_score_task = Scheduler.AddDelayed(() => localRankDisplay.Beatmap = playBeatmap, local_score_debounce);
+            void updateLocalRankDisplay()
+                => localRankDisplay.Beatmap = playBeatmap;
+
+            if (Item.Model is GroupedBeatmap)
+            {
+                // The debounce is intended to reduce the number of realm notification registrations
+                // when rapidly scrolling through the song select.
+                // However, only beatmap sets with single beatmap would show the local rank display without manual expansion.
+                // So a beatmap model indicates the user is manually expanding the set,
+                // theres no frequent registrations in this case and a immediate update is better UX.
+                // You wouldn't want to see visual jumping after a delay when you just expanded a set.
+                updateLocalRankDisplay();
+            }
+            else
+            {
+                // Realm notification registration is VERY expensive and can NOT be done asynchronously.
+                // Generally we would cache the local scores for all beatmaps beforehand,
+                // but since we are just patching lazer's code instead of rewriting the whole song select, 
+                // we will just debounce the updates to reduce the number of registrations.
+                local_score_task = Scheduler.AddDelayed(updateLocalRankDisplay, local_score_debounce);
+            }
         }
 
         if (displayPolicy.CoverBeatmap is not null && beatmaps is not null)
