217eb85b63e467c256ca052ce051cd3b81ecb23e
Caiyi Hsu <Cai1Hsu@outlook.com>
2026-02-06 04:08:14 +0800

    fix: ensure proper disposal of HttpResponseMessage in LegacyLocalisationManager

--- osu.Plugin.LegacyExperience/Localisations/LegacyLocalisationManager.cs
+++ osu.Plugin.LegacyExperience/Localisations/LegacyLocalisationManager.cs
@@ -143,7 +143,7 @@ public partial class LegacyLocalisationManager : Component
 
             try
             {
-                var response = await httpClient.GetAsync(url);
+                using var response = await httpClient.GetAsync(url);
 
                 if (response.StatusCode == HttpStatusCode.NotFound)
                 {
