99dd96cf503bb7bf99cdef0ee354e7c79a21c9f7
Caiyi Hsu <Cai1Hsu@outlook.com>
2026-02-08 01:43:37 +0800

    fix: correct return tag formatting in IModHoverManager documentation

--- osu.Plugin.LegacyExperience/Mods/IModHoverManager.cs
+++ osu.Plugin.LegacyExperience/Mods/IModHoverManager.cs
@@ -5,6 +5,6 @@ public interface IModHoverManager
     /// <summary>
     /// Requests the hover sample to be played. This is used to debounce hover samples when hovering over multiple mod icons in quick succession.
     /// </summary>
-    /// <returns>>Whether the hover sample should be played.</returns>
+    /// <returns>Whether the hover sample should be played.</returns>
     bool RequestHoverSample();
 }
