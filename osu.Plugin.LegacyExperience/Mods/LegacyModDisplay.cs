952c4a991fa935b301f5a3bbe9ad5847bf46acad
Caiyi Hsu <Cai1Hsu@outlook.com>
2026-02-07 20:47:21 +0800

    feat: implement LegacyModDisplay class for rendering legacy mod textures

---
+++ osu.Plugin.LegacyExperience/Mods/LegacyModDisplay.cs
@@ -0,0 +1,41 @@
+using System.Diagnostics;
+using osu.Framework.Allocation;
+using osu.Framework.Graphics.Sprites;
+using osu.Framework.Graphics.Textures;
+using osu.Game.Plugins;
+using osu.Game.Skinning;
+
+namespace osu.Plugin.LegacyExperience.Mods;
+
+public partial class LegacyModDisplay : Sprite
+{
+    public LegacyMod Mod { get; }
+
+    public LegacyModDisplay(LegacyMod mod)
+    {
+        Mod = mod;
+    }
+
+    [Resolved]
+    private TextureStore textures { get; set; } = null!;
+
+    [Resolved]
+    private ISkinSource? skin { get; set; }
+
+    [BackgroundDependencyLoader]
+    private void load()
+    {
+        updateTexture();
+        skin?.SourceChanged += updateTexture;
+    }
+
+    private void updateTexture()
+    {
+        var texture = skin.GetSkinTexture($"selection-mod-{Mod}", textures, "UI");
+
+        Debug.Assert(texture is not null); // we've packed default icons, so this should never be null.
+
+        Texture = texture;
+        Size = Texture.DisplaySize;
+    }
+}
