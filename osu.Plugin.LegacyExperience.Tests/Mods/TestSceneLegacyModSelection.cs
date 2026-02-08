6e6a85c4da86464efa6ecacb27fe685cec3364a2
Caiyi Hsu <Cai1Hsu@outlook.com>
2026-02-08 19:04:47 +0800

    test: remove mod groups

--- osu.Plugin.LegacyExperience.Tests/Mods/TestSceneLegacyModSelection.cs
+++ osu.Plugin.LegacyExperience.Tests/Mods/TestSceneLegacyModSelection.cs
@@ -37,47 +37,6 @@ public partial class TestSceneLegacyModSelection : LocalSkinTestScene
             content.Add(dialog = new LegacyModSelection()
             {
                 MultiplierText = { Text = "Score Multiplier: 1.00x" },
-                ReductionGroup =
-                {
-                    Mods =
-                    {
-                        Children = new Drawable[]
-                        {
-                            new LegacyModSwitch(new[] { LegacyMod.Easy }),
-                            new LegacyModSwitch(new[] { LegacyMod.NoFail }),
-                            new LegacyModSwitch(new[] { LegacyMod.HalfTime }),
-                        }
-                    }
-                },
-                IncreaseGroup =
-                {
-                    Mods =
-                    {
-                        Children = new Drawable[]
-                        {
-                            new LegacyModSwitch(new[] { LegacyMod.HardRock }),
-                            new LegacyModSwitch(new[] { LegacyMod.SuddenDeath, LegacyMod.Perfect }),
-                            new LegacyModSwitch(new[] { LegacyMod.DoubleTime, LegacyMod.Nightcore }),
-                            new LegacyModSwitch(new[] { LegacyMod.Hidden }),
-                            new LegacyModSwitch(new[] { LegacyMod.Flashlight }),
-                        }
-                    }
-                },
-                SpecialGroup =
-                {
-                    Mods =
-                    {
-                        Children = new Drawable[]
-                        {
-                            new LegacyModSwitch(new[] { LegacyMod.Relax }),
-                            new LegacyModSwitch(new[] { LegacyMod.Relax2 }),
-                            new LegacyModSwitch(new[] { LegacyMod.Target }),
-                            new LegacyModSwitch(new[] { LegacyMod.SpunOut }),
-                            new LegacyModSwitch(new[] { LegacyMod.Autoplay, LegacyMod.Cinema }),
-                            new LegacyModSwitch(new[] { LegacyMod.ScoreV2 }),
-                        }
-                    }
-                }
             });
             dialog.Show();
         });
