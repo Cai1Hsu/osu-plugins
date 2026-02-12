29b4f7886e0148b661d4647ae170ca9045266be4
Caiyi Hsu <Cai1Hsu@outlook.com>
2026-02-12 22:18:06 +0800

    feat: implement TestSceneAudioEngine for audio engine testing

---
+++ osu.Plugin.LegacyExperience.Tests/Audio/TestSceneAudioEngine.cs
@@ -0,0 +1,71 @@
+using NUnit.Framework;
+using osu.Framework.Allocation;
+using osu.Framework.Bindables;
+using osu.Framework.Extensions;
+using osu.Framework.Graphics;
+using osu.Framework.Input.Events;
+using osu.Game.Graphics.Containers;
+using osu.Game.Skinning;
+using osu.Plugin.LegacyExperience.Audio;
+using osuTK.Input;
+
+namespace osu.Plugin.LegacyExperience.Tests.Audio;
+
+public partial class TestSceneAudioEngine : LocalSkinTestScene
+{
+    private OsuTextFlowContainer infoText = null!;
+    private AudioEngine audioEngine = null!;
+
+    private readonly Bindable<LegacySample> clickSample = new Bindable<LegacySample>(LegacySample.menuhit);
+
+    [BackgroundDependencyLoader]
+    private void load()
+    {
+        Child = new SkinProvidingContainer(new DefaultLegacySkin(this))
+        {
+            RelativeSizeAxes = Axes.Both,
+            Children = new Drawable[]
+            {
+                infoText = new OsuTextFlowContainer(s => s.Font = s.Font.With(size: 16))
+                {
+                    Anchor = Anchor.TopCentre,
+                    Origin = Anchor.TopCentre,
+                    AutoSizeAxes = Axes.Both,
+                },
+                audioEngine = new AudioEngine()
+            }
+        };
+
+        clickSample.BindValueChanged(_ => updateInfoText(), true);
+    }
+
+    private void updateInfoText()
+    {
+        infoText.Text = $"Current Click Sample: {clickSample.Value.GetDescription()} (Press 1 to play)";
+    }
+
+    protected override bool OnKeyDown(KeyDownEvent e)
+    {
+        if (!e.Repeat)
+        {
+            switch (e.Key)
+            {
+                case Key.Number1:
+                    audioEngine.Click(sample: clickSample.Value);
+                    return true;
+            }
+        }
+        return base.OnKeyDown(e);
+    }
+
+    [Test]
+    public void TestClickEffect()
+    {
+        foreach (var sample in Enum.GetValues<LegacySample>())
+        {
+            AddStep($"Set {sample.GetDescription()}", () => clickSample.Value = sample);
+        }
+
+        AddStep("Click", () => audioEngine.Click(sample: clickSample.Value));
+    }
+}
