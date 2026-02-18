ebe6f80683dd31ec8bef9e871f26d1423a34324a
Caiyi Hsu <Cai1Hsu@outlook.com>
2026-02-19 02:38:36 +0800

    feat: implement AmplitudesProvider and MenuVisualisation components

---
+++ osu.Plugin.LegacyExperience/Screens/Menu/AmplitudesProvider.cs
@@ -0,0 +1,93 @@
+using osu.Framework.Allocation;
+using osu.Framework.Audio.Track;
+using osu.Framework.Graphics;
+using osu.Framework.Utils;
+using osu.Game.Beatmaps;
+using osu.Game.Overlays;
+using static osu.Plugin.LegacyExperience.Screens.Menu.IAmplitudesProvider;
+
+namespace osu.Plugin.LegacyExperience.Screens.Menu;
+
+public partial class AmplitudesProvider : Component, IAmplitudesProvider
+{
+    public ReadOnlySpan<float> Data => data;
+
+    private readonly float[] data = new float[SampleSize];
+
+    [Resolved]
+    private IBeatSyncProvider beatSyncProvider { get; set; } = null!;
+
+    public float Epicness { get; set; } = 1;
+
+    [Resolved]
+    private MusicController musicController { get; set; } = null!;
+
+    public bool UseTrackAmplitudes { get; set; } = true;
+
+    protected override void Update()
+    {
+        base.Update();
+
+        var currentTrack = musicController.CurrentTrack;
+        var extendedTime = currentTrack.HasCompleted ||
+            currentTrack.CurrentTime <= 0 ||
+            currentTrack.CurrentTime >= currentTrack.Length;
+
+        if (!UseTrackAmplitudes && (!currentTrack.IsRunning || extendedTime))
+        {
+            applyShirnkage();
+        }
+        else
+        {
+            applyTrackAmplitudes();
+        }
+    }
+
+    private void applyShirnkage()
+    {
+        const double sixty_fps = 1000.0 / 60;
+
+        double frameRatio = Clock.ElapsedFrameTime / sixty_fps;
+        double factor = Math.Pow(0.95, frameRatio);
+
+        for (int i = 0; i < SampleSize; i++)
+        {
+            double v = data[i] * factor;
+
+            if (v < 0.01f)
+                v = 0;
+
+            data[i] = (float)v;
+        }
+    }
+
+    private void applyTrackAmplitudes()
+    {
+        var source = beatSyncProvider.CurrentAmplitudes.FrequencyAmplitudes.Span;
+
+        // FFT512 -> FFT2048 expansion, osu!framework hardcoded 256 samples for FFT512
+        const int expand_factor = SampleSize / ChannelAmplitudes.AMPLITUDES_SIZE;
+
+        // TODO: may use better interpolation method if needed, but looks good to me for now. 
+        for (int i = 0; i < ChannelAmplitudes.AMPLITUDES_SIZE; i++)
+        {
+            float prev = source[(i - 1 + ChannelAmplitudes.AMPLITUDES_SIZE) % ChannelAmplitudes.AMPLITUDES_SIZE];
+            float v = source[i];
+            float next = source[(i + 1) % ChannelAmplitudes.AMPLITUDES_SIZE];
+
+            int baseIndex = i * expand_factor;
+
+            // peak boosting, may look better when the source is too flat
+            data[baseIndex] = (float)Interpolation.Lerp(prev, v, 0.5f) * 0.2f;
+            data[baseIndex + 1] = (float)Interpolation.Lerp(v, next, 0.75f) * 0.6f;
+            data[baseIndex + 2] = v;
+            data[baseIndex + 3] = (float)Interpolation.Lerp(v, next, 0.25f) * 0.6f;
+        }
+
+        if (Epicness != 1)
+        {
+            for (int i = 0; i < SampleSize; i++)
+                data[i] *= Epicness;
+        }
+    }
+}
---
+++ osu.Plugin.LegacyExperience/Screens/Menu/MenuVisualisation.cs
@@ -0,0 +1,151 @@
+using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Sprites;
+using osu.Framework.Graphics.Textures;
+using osu.Framework.Layout;
+using osu.Game.Plugins;
+using osuTK;
+
+namespace osu.Plugin.LegacyExperience.Screens.Menu;
+
+public partial class MenuVisualisation : CompositeDrawable
+{
+    [Resolved]
+    private IAmplitudesProvider amplitudes { get; set; } = null!;
+
+    // used in intro stage
+    public static readonly Colour4 intro_colour = new Colour4(0, 114, 255, 255);
+
+    public static readonly Colour4 default_colour = new Colour4(128, 128, 160, 255);
+
+    private readonly Sprite[] sprites = new Sprite[IAmplitudesProvider.SampleSize];
+
+    private LayoutValue radiusLayout = new LayoutValue(Invalidation.None);
+
+    public float Radius
+    {
+        get => field;
+        set
+        {
+            if (field == value)
+                return;
+
+            field = value;
+            radiusLayout.Invalidate();
+        }
+    }
+
+    public MenuVisualisation()
+    {
+        AddLayout(radiusLayout);
+    }
+
+    [BackgroundDependencyLoader]
+    private void load(TextureStore textures)
+    {
+        Colour = default_colour;
+
+        var texture = textures.GetAutoSized("UI/menu-vis");
+
+        // Currently using sprites, may migrate to custom draw node if performance becomes an issue, but seems fine for now.
+        for (int i = 0; i < sprites.Length; i++)
+        {
+            // negative to make the direction agree with stable, don't know why.
+            double angle = -Math.Tau * (0.4 + (double)i / sprites.Length * (Overshoot * 0.5));
+            directions[i] = new Vector2((float)Math.Cos(angle), (float)Math.Sin(angle));
+
+            AddInternal(sprites[i] = new Sprite
+            {
+                Anchor = Anchor.Centre,
+                Origin = Anchor.CentreLeft,
+                Alpha = 0.2f,
+                Blending = BlendingParameters.Additive,
+                Scale = new Vector2(0, 0.5f),
+                Texture = texture,
+                Rotation = (float)(angle * 180 / Math.PI)
+            });
+        }
+    }
+
+    private readonly Vector2[] directions = new Vector2[IAmplitudesProvider.SampleSize];
+
+    protected override void Update()
+    {
+        base.Update();
+
+        if (!radiusLayout.IsValid)
+        {
+            var value = Radius;
+
+            for (int i = 0; i < sprites.Length; i++)
+            {
+                sprites[i].Position = directions[i] * value * LegacyExperiencePlugin.StableRatio;
+            }
+
+            radiusLayout.Validate();
+        }
+
+        updateVisualisation(Clock.ElapsedFrameTime);
+    }
+
+    public float MaxAlpha { get; set; } = 0.4f;
+
+    public float Overshoot { get; set; } = 8f;
+
+    private double startOffset;
+
+    private double columnCurrentMilliseconds;
+
+    private void updateVisualisation(double elapsed)
+    {
+        if (elapsed > 1000)
+            return;
+
+        while (elapsed > 0.0)
+        {
+            columnCurrentMilliseconds += elapsed;
+            double step = Math.Max(0.0, columnCurrentMilliseconds - 10.0);
+
+            elapsed -= step;
+
+            updateColumn(elapsed);
+
+            if (step != 0.0)
+            {
+                columnCurrentMilliseconds = 0.0;
+                startOffset = (startOffset + 50.0) % sprites.Length;
+                elapsed = step;
+                continue;
+            }
+            break;
+        }
+    }
+
+    private void updateColumn(double elapsed)
+    {
+        const double sixty_fps = 1000.0 / 60;
+
+        double frameRatio = Math.Pow(0.95, elapsed / sixty_fps);
+
+        for (int i = 0; i < sprites.Length; i++)
+        {
+            Sprite sprite = sprites[i];
+
+            Vector2 scale = sprite.Scale;
+            scale.X = Math.Max(scale.X, amplitudes.Data[(int)((i + startOffset) % amplitudes.Data.Length)] * 3) * (float)frameRatio;
+
+            if (scale.X < 0.01f)
+            {
+                scale.X = 0f;
+                sprite.Alpha = 0f;
+            }
+            else
+            {
+                sprite.Alpha = Math.Max(0f, MaxAlpha * Math.Min(1f, (scale.X - 0.04f) / 0.08f));
+            }
+
+            sprite.Scale = scale;
+        }
+    }
+}
