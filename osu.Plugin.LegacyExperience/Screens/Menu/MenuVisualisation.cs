eef00d4a3de9f045aea38c71fc7707ba56a42b44
Caiyi Hsu <Cai1Hsu@outlook.com>
2026-02-19 05:17:13 +0800

    rewrite MenuVisualisation with custom rendering pipeline

--- osu.Plugin.LegacyExperience/Screens/Menu/MenuVisualisation.cs
+++ osu.Plugin.LegacyExperience/Screens/Menu/MenuVisualisation.cs
@@ -1,15 +1,17 @@
+using System.Diagnostics;
 using osu.Framework.Allocation;
 using osu.Framework.Graphics;
-using osu.Framework.Graphics.Containers;
-using osu.Framework.Graphics.Sprites;
+using osu.Framework.Graphics.Primitives;
+using osu.Framework.Graphics.Rendering;
+using osu.Framework.Graphics.Rendering.Vertices;
+using osu.Framework.Graphics.Shaders;
 using osu.Framework.Graphics.Textures;
-using osu.Framework.Layout;
 using osu.Game.Plugins;
 using osuTK;
 
 namespace osu.Plugin.LegacyExperience.Screens.Menu;
 
-public partial class MenuVisualisation : CompositeDrawable
+public partial class MenuVisualisation : Drawable
 {
     [Resolved]
     private IAmplitudesProvider amplitudes { get; set; } = null!;
@@ -19,79 +21,45 @@ public partial class MenuVisualisation : CompositeDrawable
 
     public static readonly Colour4 default_colour = new Colour4(128, 128, 160, 255);
 
-    private readonly Sprite[] sprites = new Sprite[IAmplitudesProvider.SampleSize];
+    private readonly VisualisationColumn[] columns = new VisualisationColumn[IAmplitudesProvider.SampleSize];
 
-    private LayoutValue radiusLayout = new LayoutValue(Invalidation.None);
+    public float Radius { get; set; }
 
-    public float Radius
-    {
-        get => field;
-        set
-        {
-            if (field == value)
-                return;
-
-            field = value;
-            radiusLayout.Invalidate();
-        }
-    }
+    private Texture texture = null!;
+    private IShader shader = null!;
 
     public MenuVisualisation()
     {
-        AddLayout(radiusLayout);
+        Blending = BlendingParameters.Additive;
     }
 
     [BackgroundDependencyLoader]
-    private void load(TextureStore textures)
+    private void load(TextureStore textures, ShaderManager shaders)
     {
+        texture = textures.GetAutoSized("UI/menu-vis")!;
+
+        // this is a built-in texture and should always be present, so it's fine to throw if it isn't found.
+        Debug.Assert(texture != null, "Failed to load menu visualisation texture.");
+
+        shader = shaders.Load(VertexShaderDescriptor.TEXTURE_2, FragmentShaderDescriptor.TEXTURE);
+
         Colour = default_colour;
-
-        var texture = textures.GetAutoSized("UI/menu-vis");
-
-        // Currently using sprites, may migrate to custom draw node if performance becomes an issue, but seems fine for now.
-        for (int i = 0; i < sprites.Length; i++)
-        {
-            // negative to make the direction agree with stable, don't know why.
-            double angle = -Math.Tau * (0.4 + (double)i / sprites.Length * (Overshoot * 0.5));
-            directions[i] = new Vector2((float)Math.Cos(angle), (float)Math.Sin(angle));
-
-            AddInternal(sprites[i] = new Sprite
-            {
-                Anchor = Anchor.Centre,
-                Origin = Anchor.CentreLeft,
-                Alpha = 0.2f,
-                Blending = BlendingParameters.Additive,
-                Scale = new Vector2(0, 0.5f),
-                Texture = texture,
-                Rotation = (float)(angle * 180 / Math.PI)
-            });
-        }
     }
 
-    private readonly Vector2[] directions = new Vector2[IAmplitudesProvider.SampleSize];
+    protected override DrawNode CreateDrawNode() => new MenuVisualisationDrawNode(this);
 
     protected override void Update()
     {
         base.Update();
 
-        if (!radiusLayout.IsValid)
-        {
-            var value = Radius;
-
-            for (int i = 0; i < sprites.Length; i++)
-            {
-                sprites[i].Position = directions[i] * value * LegacyExperiencePlugin.StableRatio;
-            }
-
-            radiusLayout.Validate();
-        }
-
         updateVisualisation(Clock.ElapsedFrameTime);
+
+        Invalidate(Invalidation.DrawNode);
     }
 
     public float MaxAlpha { get; set; } = 0.4f;
 
-    public float Overshoot { get; init; } = 8f;
+    public float Overshoot { get; set; } = 8f;
 
     private double startOffset;
 
@@ -118,7 +86,7 @@ public partial class MenuVisualisation : CompositeDrawable
                 break;
 
             columnCurrentMilliseconds = 0.0;
-            startOffset = (startOffset + 50.0) % sprites.Length;
+            startOffset = (startOffset + 50.0) % columns.Length;
         }
     }
 
@@ -126,26 +94,119 @@ public partial class MenuVisualisation : CompositeDrawable
     {
         const double sixty_fps = 1000.0 / 60;
 
-        double frameRatio = Math.Pow(0.95, elapsed / sixty_fps);
+        float frameRatio = (float)Math.Pow(0.95, elapsed / sixty_fps);
 
-        for (int i = 0; i < sprites.Length; i++)
+        for (int i = 0; i < columns.Length; i++)
         {
-            Sprite sprite = sprites[i];
+            ref VisualisationColumn column = ref columns[i];
 
-            Vector2 scale = sprite.Scale;
-            scale.X = Math.Max(scale.X, amplitudes.Data[(int)((i + startOffset) % amplitudes.Data.Length)] * 3) * (float)frameRatio;
+            column.ScaleX = Math.Max(column.ScaleX, amplitudes.Data[(int)((i + startOffset) % amplitudes.Data.Length)] * 3) * frameRatio;
 
-            if (scale.X < 0.01f)
+            if (column.ScaleX < 0.01f)
             {
-                scale.X = 0f;
-                sprite.Alpha = 0f;
+                column.ScaleX = 0f;
             }
             else
             {
-                sprite.Alpha = Math.Max(0f, MaxAlpha * Math.Min(1f, (scale.X - 0.04f) / 0.08f));
+                column.Alpha = Math.Max(0f, MaxAlpha * Math.Min(1f, (column.ScaleX - 0.04f) / 0.08f));
+            }
+        }
+    }
+
+    private struct VisualisationColumn
+    {
+        public float Alpha;
+        public float ScaleX;
+    }
+
+    private class MenuVisualisationDrawNode : DrawNode
+    {
+        protected new MenuVisualisation Source => (MenuVisualisation)base.Source;
+
+        private readonly VisualisationColumn[] columns = new VisualisationColumn[IAmplitudesProvider.SampleSize];
+
+        public MenuVisualisationDrawNode(MenuVisualisation source)
+            : base(source)
+        {
+        }
+
+        private float overshoot;
+
+        private Texture texture = null!;
+        private IShader shader = null!;
+
+        private float radius;
+
+        private Vector2 center;
+
+        public override void ApplyState()
+        {
+            base.ApplyState();
+
+            overshoot = Source.Overshoot;
+            radius = Source.Radius;
+            shader = Source.shader;
+            texture = Source.texture;
+            center = Source.DrawSize / 2;
+            Source.columns.CopyTo(columns, 0);
+        }
+
+        private IVertexBatch<TexturedVertex2D>? vertexBatch;
+
+        const float scaleY = 0.5f;
+
+        protected override void Draw(IRenderer renderer)
+        {
+            base.Draw(renderer);
+
+            vertexBatch ??= renderer.CreateQuadBatch<TexturedVertex2D>(columns.Length, 1);
+
+            shader.Bind();
+            renderer.PushLocalMatrix(DrawInfo.Matrix);
+
+            for (int i = 0; i < columns.Length; i++)
+            {
+                VisualisationColumn column = columns[i];
+
+                if (column.Alpha <= 0 || column.ScaleX <= 0)
+                    continue;
+
+                // negative to make the direction agree with stable, don't know why.
+                float rotation = -MathF.Tau * (0.4f + (float)i / columns.Length * (overshoot * 0.5f));
+
+                float rotationCos = MathF.Cos(rotation);
+                float rotationSin = MathF.Sin(rotation);
+
+                Vector2 origin = center + new Vector2(rotationCos, rotationSin) * radius;
+                Vector2 size = texture.DisplaySize * new Vector2(column.ScaleX, scaleY);
+
+                float halfThickness = size.Y / 2;
+
+                Vector2 outwardDirection = new Vector2(rotationCos, rotationSin) * size.X;
+                Vector2 tangentDirection = new Vector2(-rotationSin, rotationCos) * halfThickness;
+
+                var quad = new Quad(
+                    origin - tangentDirection,
+                    origin + outwardDirection - tangentDirection,
+                    origin + tangentDirection,
+                    origin + outwardDirection + tangentDirection
+                );
+
+                var colour = DrawColourInfo.Colour.MultiplyAlpha(column.Alpha);
+
+                renderer.DrawQuad(texture, quad, colour, null, vertexBatch.AddAction);
             }
 
-            sprite.Scale = scale;
+            renderer.PopLocalMatrix();
+
+            shader.Unbind();
+        }
+
+        protected override void Dispose(bool isDisposing)
+        {
+            base.Dispose(isDisposing);
+
+            vertexBatch?.Dispose();
         }
     }
 }
