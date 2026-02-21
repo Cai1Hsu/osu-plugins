986c2db84d847486b7c37b43013a58e00cb8cfb8
Caiyi Hsu <Cai1Hsu@outlook.com>
2026-02-21 09:28:28 +0800

    fix: assign explicit values to SeasonalEvents enum members

--- osu.Plugin.LegacyExperience/Seasonal/SeasonalEvents.cs
+++ osu.Plugin.LegacyExperience/Seasonal/SeasonalEvents.cs
@@ -7,8 +7,8 @@ namespace osu.Plugin.LegacyExperience.Seasonal;
 [Flags]
 public enum SeasonalEvents
 {
-    None,
-    Halloween,
-    Christmas,
-    Summer
+    None = 0,
+    Halloween = 1 << 0,
+    Christmas = 1 << 1,
+    Summer = 1 << 2
 }
