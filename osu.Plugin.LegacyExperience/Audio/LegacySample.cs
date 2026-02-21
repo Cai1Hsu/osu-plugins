c7f314e753a9339ad7a0cd9721a347b5175aab71
Caiyi Hsu <Cai1Hsu@outlook.com>
2026-02-22 04:37:27 +0800

    feat: add new legacy samples for menu interactions

--- osu.Plugin.LegacyExperience/Audio/LegacySample.cs
+++ osu.Plugin.LegacyExperience/Audio/LegacySample.cs
@@ -29,4 +29,14 @@ public enum LegacySample
     sectionfail,
     [Description("heartbeat")]
     heartbeat,
+    [Description("menuback")]
+    menuback,
+    [Description("menu-edit-click")]
+    menu_edit_click,
+    [Description("menu-freeplay-click")]
+    menu_freeplay_click,
+    [Description("menu-multiplayer-click")]
+    menu_multiplayer_click,
+    [Description("menu-play-click")]
+    menu_play_click,
 }
