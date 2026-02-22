86b9c03913878526e0ecf57eb1becc681d385b7d
Caiyi Hsu <Cai1Hsu@outlook.com>
2026-02-22 22:58:10 +0800

    fix: ignore keybinds with modifiers to prevent interference with user keybinds

--- osu.Plugin.LegacyExperience/Screens/Menu/ButtonSystem.cs
+++ osu.Plugin.LegacyExperience/Screens/Menu/ButtonSystem.cs
@@ -302,6 +302,11 @@ public partial class ButtonSystem : Container
 
     protected override bool OnKeyDown(KeyDownEvent e)
     {
+        // ignore keybinds with modifiers to avoid interfering with user keybinds (e.g. ctrl+e to open editor)
+        // also we don't want to stop idle state via modifier keys since that would be really annoying
+        if (e.AltPressed || e.ControlPressed || e.ShiftPressed || e.SuperPressed)
+            return base.OnKeyDown(e);
+        
         if (state.Value is ButtonSystemState.Collapsed)
             updateState(ButtonSystemState.Main);
 
